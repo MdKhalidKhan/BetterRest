d1286ad60b1b388bf6eb6be6790151cb5f748d85
Raj <rajkhulna@outlook.com>
2022-03-31 16:22:58 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Khalid Khan on 31/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
